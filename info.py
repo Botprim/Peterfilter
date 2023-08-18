60961ff5a10692b62b13b863fb6f8dea8bf7234f
Botprim <123348827+Botprim@users.noreply.github.com>
2023-08-18 09:22:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,11 +49,11 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', '')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Omegalinks.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'b2582f8c38a9c76c2315b44f7ba039ea7196e551')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'Omegalinks.in')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', 'b2582f8c38a9c76c2315b44f7ba039ea7196e551')
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
